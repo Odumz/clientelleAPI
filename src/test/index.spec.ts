0edfdcdd3f848087d37fed61b088dc3468a99d0b
Odumz <odumz20@gmail.com>
2021-09-22 03:02:16 +0100

    get and post route tests done

--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -48,10 +48,8 @@ describe('Get all route tests', () => {
 });
 
 describe('Post route tests', () => {
-    beforeEach((done) => {
-        console.log('clearing db')
-        
-        Client.remove({}, (err) => {
+    beforeEach((done) => {        
+        Client.deleteMany({}, (err) => {
             done();
         });
     });
@@ -108,6 +106,80 @@ describe('Post route tests', () => {
                 name: 'Test1',
                 email: 'test1@taast.test',
                 phone: '13649152557'
+            });
+        expect(res.status).to.be.equal(500);
+        expect(res.body).to.be.empty;
+    });
+    it('should not add data to the client data if phone number is less than 10', async () => {
+        const res = await request(app)
+            .post('/api/v1/clients/add')
+            .send({
+                name: 'Test1',
+                email: 'test1@taast.test',
+                phone: '136491527',
+                provider: ['6146ff749250f63f63671622']
+            });
+        expect(res.status).to.be.equal(500);
+        expect(res.body).to.be.empty;
+    });
+    it('should not add data to the client data if email format is wrong', async () => {
+        const res = await request(app)
+            .post('/api/v1/clients/add')
+            .send({
+                name: 'Test1',
+                email: 'test1aast.test',
+                phone: '136491523567',
+                provider: ['6146ff749250f63f63671622']
+            });
+        expect(res.status).to.be.equal(500);
+        expect(res.body).to.be.empty;
+    });
+    it('should not add data to the client data if name is less than 3 characters', async () => {
+        const res = await request(app)
+            .post('/api/v1/clients/add')
+            .send({
+                name: 'Te',
+                email: 'test1aast.test',
+                phone: '136491523567',
+                provider: ['6146ff749250f63f63671622']
+            });
+        expect(res.status).to.be.equal(500);
+        expect(res.body).to.be.empty;
+    });
+    it('should not add data to the client data if provider id is less than 24 characters', async () => {
+        const res = await request(app)
+            .post('/api/v1/clients/add')
+            .send({
+                name: 'Test1',
+                email: 'test1aast.test',
+                phone: '136491523567',
+                provider: ['6146ff7492503f63671622']
+            });
+        expect(res.status).to.be.equal(500);
+        expect(res.body).to.be.empty;
+    });
+    it('should add data to the provider data', async () => {
+        const res = await request(app)
+            .post('/api/v1/providers/add')
+            .send({
+                name: 'Test1'
+            });
+        expect(res.status).to.be.equal(201);
+        expect(res.body).not.to.be.empty;
+        expect(res.body.provider).to.be.an('object');
+    });
+    it('should not add data to the provider data if name is missing', async () => {
+        const res = await request(app)
+            .post('/api/v1/providers/add')
+            .send({});
+        expect(res.status).to.be.equal(500);
+        expect(res.body).to.be.empty;
+    });
+    it('should not add data to the provider data if name is less than 3 characters', async () => {
+        const res = await request(app)
+            .post('/api/v1/providers/add')
+            .send({
+                name: 'Te'
             });
         expect(res.status).to.be.equal(500);
         expect(res.body).to.be.empty;
